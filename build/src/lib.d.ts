1835c9788989fa41551b0b2b58c8b41fc1761249
repinvv <repin.vladv@gmail.com>
2017-10-15 21:33:12 +0300

    getting exported names

--- build/src/lib.d.ts
+++ build/src/lib.d.ts
@@ -1,1 +1,2 @@
 export { collectFolders } from './impl/collectFolders';
+export { getExportedNames } from './impl/getExportedNames';
--- build/src/lib.js
+++ build/src/lib.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var collectFolders_1 = require("./impl/collectFolders");
 exports.collectFolders = collectFolders_1.collectFolders;
+var getExportedNames_1 = require("./impl/getExportedNames");
+exports.getExportedNames = getExportedNames_1.getExportedNames;
 //# sourceMappingURL=lib.js.map
