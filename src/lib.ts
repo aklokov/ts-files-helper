3defcb75cb796af9f5e70d9c50779b64c09cb752
repinvv <repin.vladv@gmail.com>
2017-12-03 15:10:55 +0300

    corrected typyngs export

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
 export { collectFolders } from './impl/collectFolders';
 export { getExportedNames } from './impl/getExportedNames';
 export { parseImports } from './impl/parseImports';
+export * from './interface';
